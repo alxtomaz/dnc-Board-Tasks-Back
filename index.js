1d7ff1672f99c5c6aa70dffb46f56fda4f5f71d3
alxtomaz <alextomaz70@gmail.com>
2023-10-29 17:39:16 -0300

    criando a documentação README.MD e moveu o codigo principaç para a pasta src

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors'); // adicionado alex
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger/swagger_output.json');
-const swaggerOptions = {customCssUrl: '/swagger-ui.css'};
-
-var usersRouter = require('./routes/users');
-
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors"); // adicionado alex
+const swaggerUi = require("swagger-ui-express");
+const routes = require("./src/routes");
+const swaggerOptions = { customCssUrl: "/swagger-ui.css" };
 var app = express();
-require('dotenv').config(); // adicionado alex
+require("dotenv").config(); // adicionado alex
 
+
+//config express 
 app.use(cors()); // evitar problmeas de integração de backend e frontend
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
-app.use('/users', usersRouter);
 
-if (process.env.NODE_ENV !== 'test') {
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+//configuração swagger documentação
+if (process.env.NODE_ENV !== "test") {
+  const swaggerFile = require("./swagger/swagger_output.json");
+  app.get("/", (req, res) => {
+    /* #swagger.ignore = true */ res.redirect("/doc");
+  });
+  app.use(
+    "/doc",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, swaggerOptions)
+  );
+}
+
+
+//rotas endpoins api
+routes(app);
+
+
+//inicialização do servidor
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
 module.exports = app;
